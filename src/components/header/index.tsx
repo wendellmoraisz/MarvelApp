e5ab4d3cf1a02cb10d3b3a9a2217bfa1ef52b5c7
Wendell <wendellmorais3319@gmail.com>
2022-06-28 20:56:46 -0300

    feat: componente header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,13 @@
+import * as S from "./styles"
+
+const Header = () => {
+
+    return (
+        <S.Wrapper>
+            <h1>Marvel App</h1>
+            <S.Input type="text" name="" placeholder="Search your favorite character =D" />
+        </S.Wrapper>
+    );
+}
+
+export default Header;
---
+++ src/components/header/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.header`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    h1 {
+        font-size: 4rem;
+        background: #F0131E;
+        color: #fff;
+        padding: 5px;
+    }
+`;
+
+export const Input = styled.input`
+    width: 400px;
+    padding: 10px;
+    border-radius: 10px;
+    font-size: 1.2rem;
+    background: transparent;
+    border: none;
+    box-shadow: 1px 1px 5px 1px #3c3c3c;
+    outline: 0;
+    color: #fff;
+`;

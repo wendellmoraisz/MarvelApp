ca9aa1477a7c003dd164e3ec84286f4a83264205
Wendell <wendellmorais3319@gmail.com>
2022-06-29 12:00:11 -0300

    styles: adicionando fonte local

--- src/global/globalStyle.ts
+++ src/global/globalStyle.ts
@@ -1,11 +1,18 @@
 import { createGlobalStyle } from "styled-components";
+import Marvel from "../assets/fonts/Marvel.ttf";
 
 export const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: 'Marvel';
+        src: url(${Marvel});
+    }
+
     * {
         margin: 0;
         padding: 0;
-        background-color: #1e1e1e;;
-        font-family: sans-serif;
+        background-color: #1e1e1e;
+        font-family: 'Marvel', sans-serif;
+        text-transform: uppercase;
     }
 
     body {

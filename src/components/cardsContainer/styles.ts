7092fe359bce3358655f1c8798e32c2374c668f5
Wendell <wendellmorais3319@gmail.com>
2022-06-29 11:05:23 -0300

    styles: estilizando scrollbar do container de personagens

--- src/components/cardsContainer/styles.ts
+++ src/components/cardsContainer/styles.ts
@@ -1,16 +1,25 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-width: 80vw;
-height: 70vh;
-padding: 15px;
-margin: 30px auto;
-background: #222;
-display: flex;
-justify-content: space-evenly;
-align-items: center;
-flex-wrap: wrap;
-overflow-y: scroll;
+    width: 80vw;
+    height: 70vh;
+    padding: 15px;
+    margin: 30px auto;
+    background: #222;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-wrap: wrap;
+    overflow-y: scroll;
+
+    &::-webkit-scrollbar{
+        background-color: #151515;
+        width: 5px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background-color: #f0131e;
+    }
 
     h2  {
         background-color: transparent;

4c9f866c276fbdab1ab5f35d10732cd70c82eb63
Wendell <wendellmorais3319@gmail.com>
2022-08-08 11:21:49 -0300

    alterando url da api

--- src/providers/CharactersProvider.tsx
+++ src/providers/CharactersProvider.tsx
@@ -20,7 +20,7 @@ const CharactersProvider = ({ children }: Props) => {
         const [publicKey, privateKey] = [process.env.REACT_APP_VERCEL_PUBLIC_API_KEY, process.env.REACT_APP_VERCEL_PRIVATE_API_KEY as string];
         const time = Number(new Date());
         const hash = md5(time + privateKey + publicKey);
-        const response = await axios.get(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${characterName}&ts=${time}&apikey=${publicKey}&hash=${hash}`);
+        const response = await axios.get(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${characterName}&ts=${time}&apikey=${publicKey}&hash=${hash}`);
 
         const characters = response.data.data.results;
         setCharactersState(characters.map((character:CharacterData) => ({
